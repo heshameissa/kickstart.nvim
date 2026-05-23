21d639830c5022b7fc1fd367ada9850022e198ae
Hesham Eissa <hesham.magdy.eissa@gmail.com>
2026-05-23 13:57:22 -0700

    Added ensure_installed for xcode-build-server for Mason

--- init.lua
+++ init.lua
@@ -798,6 +798,7 @@ do
   local ensure_installed = vim.tbl_keys(servers or {})
   vim.list_extend(ensure_installed, {
     -- You can add other tools here that you want Mason to install
+    'xcode-build-server', -- Force Mason to sync this via Git! *
   })
 
   require('mason-tool-installer').setup { ensure_installed = ensure_installed }
